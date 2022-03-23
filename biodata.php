03ff9012133c25fe1614626a854b30608fdf0246
webprog00 <101626752+webprog00@users.noreply.github.com>
2022-03-24 02:01:37 +0700

    Delete biodata.php

--- biodata.php
+++
@@ -1,9 +0,0 @@
-<?php
-class biodata extends CI_Controller
-{
-    public function index()
-    {
-        echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Achmad Nurarie Dwi Yuliansa Putra, Saya tinggal di daerah meruya. Olahraga uang saya sukai adalah Sepak Bola";
-    }
-}
