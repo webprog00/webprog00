f70b881d00f6144b2e1f25933140772d8278c45c
webprog00 <achmad.nurariedwi@gmail.com>
2022-03-24 02:18:45 +0700

    revisi file

---
+++ bcit/application/controllers/biodata.php
@@ -0,0 +1,9 @@
+<?php
+class biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Achmad Nurarie Dwi Yuliansa Putra, Saya tinggal di daerah meruya. Olahraga uang saya sukai adalah Sepak Bola";
+    }
+}
